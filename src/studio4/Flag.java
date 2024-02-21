b18d90cd4c566dacf14b3fb0619314b1e281902b
sashagrewal <sashagrewal@localhost>
2024-02-21 14:07:35 -0600

    studio 4

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -5,7 +5,22 @@ import java.awt.Color;
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
-	public static void main(String[] args) {
-		
+	private static void mother() {
+		   StdDraw.clear();
+		   StdDraw.setPenColor(252, 3, 169);
+		   StdDraw.filledRectangle(22, 16, 22, 16);
+		   StdDraw.setPenColor(186, 3, 252);
+		   StdDraw.filledEllipse(11, 8, 3, 3);
+		}
+
+		public static void norway() {
+		   StdDraw.setXscale(0, 22);
+		   StdDraw.setYscale(0, 16);
+		   mother();
+		}
+	public static void main(String[] args) 
+	{
+	
+		norway();
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,107 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+	
+		String shapeType = in.next();
+		if (shapeType.equals ("ellipse"))
+		{
+			int R = in.nextInt();
+			int G = in.nextInt();
+			int B = in.nextInt();
+			StdDraw.setPenColor(R,G,B);
+			
+			boolean isFilled = in.nextBoolean();
+			
+			if (isFilled)
+			{
+				double x = in.nextDouble();
+				double y = in.nextDouble();
+				double halfWidth = in.nextDouble();
+				double halfHeight = in.nextDouble();
+				
+				StdDraw.filledEllipse(x, y, halfWidth, halfHeight);
+			}
+			else 
+			{
+				double x = in.nextDouble();
+				double y = in.nextDouble();
+				double halfWidth = in.nextDouble();
+				double halfHeight = in.nextDouble();
+				
+				StdDraw.ellipse(x, y, halfWidth, halfHeight);
+			}
+		}
 		
+		else if (shapeType.equals ("rectangle"))
+		{
+			int R = in.nextInt();
+			int G = in.nextInt();
+			int B = in.nextInt();
+			StdDraw.setPenColor(R,G,B);
+			
+			boolean isFilled = in.nextBoolean();
+			
+			if (isFilled)
+			{
+				double x = in.nextDouble();
+				double y = in.nextDouble();
+				double halfWidth = in.nextDouble();
+				double halfHeight = in.nextDouble();
+				
+				StdDraw.filledRectangle(x, y, halfWidth, halfHeight);
+			}
+			else 
+			{
+				double x = in.nextDouble();
+				double y = in.nextDouble();
+				double halfWidth = in.nextDouble();
+				double halfHeight = in.nextDouble();
+				
+				StdDraw.rectangle(x, y, halfWidth, halfHeight);
+			}
+		}
+		else 
+		{
+			int R = in.nextInt();
+			int G = in.nextInt();
+			int B = in.nextInt();
+			StdDraw.setPenColor(R,G,B);
+			
+			boolean isFilled = in.nextBoolean();
+			
+			if (isFilled)
+			{
+		
+				double a = in.nextDouble();
+				double b = in.nextDouble();
+				double c = in.nextDouble();
+				
+				double [] x = {a,b,c};
+				
+				double d = in.nextDouble();
+				double e = in.nextDouble();
+				double g = in.nextDouble();
+				
+				double [] y = {d,e,g};
+				
+				StdDraw.filledPolygon(x, y);
+			}
+			else 
+			{
+				double a = in.nextDouble();
+				double b = in.nextDouble();
+				double c = in.nextDouble();
+				
+				double [] x = {a,b,c};
+				
+				double d = in.nextDouble();
+				double e = in.nextDouble();
+				double g = in.nextDouble();
+				
+				double [] y = {d,e,g};
+				
+				StdDraw.polygon(x, y);
+		}
 	}
+}
 }
